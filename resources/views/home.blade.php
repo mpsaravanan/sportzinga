63daebb41f6f99761e1fcb9f9e5c28f7827e6c8b
Saravanan <mpsaravanan035@gmail.com>
2017-02-07 15:25:59 +0530

    Game changer Initial commit 2

---
+++ resources/views/home.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SportZinga: Home of Sports</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="coming soon" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<!-- js -->
+<script type="text/javascript" src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
+<!-- //js -->
+<link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" media="all" />
+<link href='//fonts.googleapis.com/css?family=Quicksand:400,300,700' rel='stylesheet' type='text/css'>
+<style type="text/css">
+	.banner{
+	background:url("{{ asset('images/banner.jpg') }}") no-repeat 0px 0px;
+	background-size:cover;
+	-webkit-background-size:cover;
+	-moz-background-size:cover;
+	-o-background-size:cover;
+	-ms-background-size:cover;
+}
+.banner-info {
+    background: url("{{ asset('images/dott.png') }}")repeat 0px 0px;
+    background-size: 4px;
+    -webkit-background-size: 4px;
+    -moz-background-size: 4px;
+    -o-background-size: 4px;
+    -ms-background-size: 4px;
+	text-align:center;
+	padding: 3em 0;
+}
+</style>
+</head>
+<body>
+<!-- banner -->
+	<div class="banner">
+		<div class="banner-info">
+			<img src="{{ asset('images/Sportzinga_logo_final2_alpha_website.png') }}" alt="SportZinga">
+			<h1><span>Under Construction</span></h1>
+			<div class="progressbars" progress="70%"></div>
+			<script src="{{ asset('js/jprogress.js') }}" type="text/javascript"></script>
+				<script>
+					// activate jprogress
+					$(".progressbars").jprogress({
+						background: "#80C340"
+					});
+				</script>
+			<h2>SportZinga is coming soon to groom the sporty side of your kid and give shape to his/her dreams with world class network of coaches and sports facilities</h2>
+			<div class="main-example">
+				<img src="{{ asset('images/Olympic_Rings_HD.png') }}" alt="SportZinga" height="60%" width="60%">
+				<div class="countdown-container" id="main-example"></div>
+				<h2>We are a group of professional sports coaches who have come together to guide kids to become great in sports. You can post any query that you might have or simply like us on our facebook page below!</h2>
+			</div>
+
+
+			<div class="social-icons">
+				<ul class="social-networks square spin-icon">
+					<li><a href="https://www.facebook.com/sportzinga/" class="icon-facebook"></a></li>
+				</ul>
+			</div>
+			<div class="copyright">
+				<p>© 2017 SportZinga Under Construction. All rights reserved </p>
+			</div>
+		</div>
+	</div>
+<!-- //banner -->
+</body>
+</html>
